9c928ad91d3a7b477f605b87a21786d340749849
Tu Nguyen danh <55817830+tund136@users.noreply.github.com>
2020-07-01 15:34:06 +0700

    Modified admin/includes/photo.php file

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -12,6 +12,17 @@ class Photo extends DbObject {
 
     public $tmp_path;
     public $upload_directory = "image";
+    public $custom_errors = array();
+    public $upload_errors_array = array(
+        UPLOAD_ERR_OK           => "There is no error.",
+        UPLOAD_ERR_INI_SIZE     => "The uploaded file exceeds the upload_max_filesize directive in php.nit.",
+        UPLOAD_ERR_FORM_SIZE    => "The uploaded file exceeds the MAX_FILE_SIZE directive that was  specified in php.init.",
+        UPLOAD_ERR_PARTIAL      => "The uploaded file was only partially uploaded.",
+        UPLOAD_ERR_NO_FILE      => "No file was uploaded.",
+        UPLOAD_ERR_NO_TMP_DIR   => "Missing a temporary folder.",
+        UPLOAD_ERR_CANT_WRITE   => "Failed to write file to disk.",
+        UPLOAD_ERR_EXTENSION    => "A PHP extension stopped the file upload."
+    );
     
 }
 ?>
