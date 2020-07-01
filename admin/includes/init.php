5b15ecd4eda8dd57d2a9193e729d30a3b81d82a4
Tu Nguyen danh <55817830+tund136@users.noreply.github.com>
2020-07-01 15:35:20 +0700

    Modified admin/includes/init.php file

--- admin/includes/init.php
+++ admin/includes/init.php
@@ -1,4 +1,10 @@
 <?php
+defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
+
+define('SITE_ROOT', DS . 'Applications' . DS . 'MAMP' . DS . 'htdocs' . DS . 'gallery');
+
+defined('INCLUDES_PATH') ? null : define('INCLUDES_PATH', SITE_ROOT . DS . 'admin' . DS . 'includes');
+
 require_once("config.php");
 require_once("database.php");
 require_once("db_object.php");
