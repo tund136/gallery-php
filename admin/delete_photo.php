5a6cf9370306c9174fb60af670f9c5de69b04eb3
Tu Nguyen danh <55817830+tund136@users.noreply.github.com>
2020-07-02 15:32:30 +0700

    Modified admin/delete_photo.php

--- admin/delete_photo.php
+++ admin/delete_photo.php
@@ -2,5 +2,16 @@
 <?php if(!$session->isSignedIn()) {redirect("login.php");} ?>
 
 <?php
-    echo "It works";
+    if(empty($_GET['photo_id'])) {
+        redirect("./photos.php");
+    }    
+
+    $photo = Photo::findById($_GET['photo_id']);
+
+    if($photo) {
+        $photo->deletePhoto();
+        redirect("photos.php");
+    } else {
+        redirect("photos.php");
+    }
 ?>
