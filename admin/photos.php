9199812e30cabaf5177035ab99c67149dab63434
Tu Nguyen danh <55817830+tund136@users.noreply.github.com>
2020-07-02 14:06:07 +0700

    Modified admin/photos.php

--- admin/photos.php
+++ admin/photos.php
@@ -1,4 +1,9 @@
 <?php include("includes/header.php"); ?>
+<?php if(!$session->isSignedIn()) {redirect("login.php");} ?>
+
+<?php
+$photos = Photo::findAll();
+?>
 
 <!-- Navigation -->
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -21,14 +26,34 @@
                     Photos
                     <small>Subheading</small>
                 </h1>
-                <ol class="breadcrumb">
-                    <li>
-                        <i class="fa fa-dashboard"></i> <a href="index.html">Dashboard</a>
-                    </li>
-                    <li class="active">
-                        <i class="fa fa-file"></i> Blank Page
-                    </li>
-                </ol>
+                
+                <div class="col-md-12">
+
+                    <table class="table table-hover"> <!-- Start of Table -->
+                        <thead>
+                            <tr>
+                                <th>Photo</th>
+                                <th>Id</th>
+                                <th>File Name</th>
+                                <th>Title</th>
+                                <th>Size</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                        <?php foreach ($photos as $photo) : ?>
+                            <tr>
+                                <td><img src="http://placehold.it/62" alt=""></td>
+                                <td><?php echo $photo->photo_id ?></td>
+                                <td><?php echo $photo->filename ?></td>
+                                <td><?php echo $photo->title ?></td>
+                                <td><?php echo $photo->size ?></td>
+                            </tr>
+                        <?php endforeach; ?>       
+                        </tbody>
+                    </table> <!-- End of Table -->
+                    
+                </div>
             </div>
         </div>
         <!-- /.row -->
