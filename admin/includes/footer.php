e4cb236a9fe2f2023fe9edd8068b021f3f0ae2e8
Tu Nguyen danh <55817830+tund136@users.noreply.github.com>
2020-07-03 11:00:21 +0700

    Import TinyMCE Editor

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -7,6 +7,9 @@
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
 
+    <!-- Tinymce Editor -->
+    <script src="https://cdn.tiny.cloud/1/slx86oz4k07r6imnyed3jf3y9w9rhpu7pdltaky61e2hmpxx/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+    <script src="js/scripts.js"></script>
 </body>
 
 </html>
