1eeddc3ddd75aa9956938c4d2bd01d47390c6a02
Tu Nguyen danh <55817830+tund136@users.noreply.github.com>
2020-07-03 11:02:59 +0700

    Add admin/edit_photo.php file

---
+++ admin/edit_photo.php
@@ -0,0 +1,114 @@
+<?php include("includes/header.php"); ?>
+<?php if(!$session->isSignedIn()) {redirect("login.php");} ?>
+
+<?php
+$message = "";
+if(empty(isset($_GET['photo_id']))) {
+    redirect("photos.php");
+} else {
+    $photo = Photo::findById($_GET['photo_id']);
+    
+    if(isset($_POST['update'])) {
+        if($photo) {
+            $photo->title = $_POST['title'];
+            $photo->description = $_POST['description'];
+
+            $photo->save();
+            $message = "Update Successfully!";
+        }
+    }
+}
+
+?>
+
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <?php include("includes/top_nav.php"); ?>
+
+    <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+    <?php include("includes/side_nav.php"); ?>
+    <!-- /.navbar-collapse -->
+</nav>
+
+<div id="page-wrapper">
+
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    Photos
+                    <small>Subheading</small>
+                </h1>
+                <form action="" method="post">
+                    <div class="col-md-8">
+                        <?php echo $message; ?>
+                        <div class="form-group">
+                            <label for="title">Title</label>
+                            <input type="text" name="title" class="form-control" value="<?php echo $photo->title; ?>">
+                        </div>
+
+                        <div class="form-group">
+                            <a class="thumbnail" href="#"><img src="<?php echo $photo->picturePath(); ?>" alt=""></a>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="description">Description</label>
+                            <textarea class="form-control" name="description" id="" cols="30" rows="10"><?php echo $photo->description; ?></textarea>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="photo-info-box">
+                            <div class="info-box-header">
+                                <h4>Save <span id="toggle" class="glyphicon glyphicon-menu-up pull-right"></span></h4>
+                            </div>
+
+                            <div class="inside">
+                                <div class="box-inner">
+                                    <p class="text">
+                                        <span class="glyphicon glyphicon-calendar"></span> Uploaded on: July 03, 2020
+                                    </p>
+
+                                    <p class="text">
+                                        Photo Id: <span class="data photo_id_box"><?php echo $photo->id; ?></span>
+                                    </p>
+
+                                    <p class="text">
+                                        Filename: <span class="data"><?php echo $photo->filename; ?></span>
+                                    </p>
+                                    <p class="text">
+                                        File Type: <span class="data"><?php echo $photo->type; ?></span>
+                                    </p>
+                                    <p class="text">
+                                        File Size: <span class="data"><?php echo $photo->size; ?></span>
+                                    </p>
+                                </div>
+
+                                <div class="info-box-footer clearfix">
+                                    <div class="info-box-delete pull-left">
+                                        <a href="delete_photo.php?photo_id=<?php echo $photo->id; ?>" class="btn btn-danger btn-lg">Delete</a>
+                                    </div>
+
+                                    <div class="info-box-update pull-right">
+                                        <input type="submit" name="update" value="Update" class="btn btn-primary btn-lg">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+        <!-- /.row -->
+
+    </div>
+    <!-- /.container-fluid -->
+
+</div>
+<!-- /#page-wrapper -->
+
+<?php include("includes/footer.php"); ?>
--- admin/photos.php
+++ admin/photos.php
@@ -43,10 +43,10 @@ $photos = Photo::findAll();
                         <tbody>
                         <?php foreach ($photos as $photo) : ?>
                             <tr>
-                                <td><img src="<?php echo $photo->picturePath(); ?>" alt="">
+                                <td><img class="admin-photo-thumbnail" src="<?php echo $photo->picturePath(); ?>" alt="">
                                     <div class="pictures_link">
                                         <a href="delete_photo.php?photo_id=<?php echo $photo->id; ?>">Delete</a>
-                                        <a href="#">Edit</a>
+                                        <a href="edit_photo.php?photo_id=<?php echo $photo->id; ?>">Edit</a>
                                         <a href="#">View</a>
                                     </div>
                                 </td>
