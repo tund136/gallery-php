11cb561759776a4d4b47daccb1cd9b7ca23d702b
Tu Nguyen danh <55817830+tund136@users.noreply.github.com>
2020-07-06 10:09:00 +0700

    Modified admin/includes/user.php file

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -2,13 +2,21 @@
 class User extends DbObject {
 
     protected static $dbTable = "users";
-    protected static $dbTableFields = array('username', 'password', 'first_name', 'last_name');
+    protected static $dbTableFields = array('username', 'password', 'first_name', 'last_name', 'user_image');
 
     public $id;
     public $username;
     public $password;
     public $first_name;
     public $last_name;
+    public $user_image;
+
+    public $upload_directory = "images";
+    public $image_placeholder = "http://placehold.it/100";
+
+    public function imagePathAndPlaceholder() {
+        return empty($this->user_image) ? $this->image_placeholder : $this->upload_directory . DS . $this->user_image;
+    }
 
     // Verify User
     public static function verifyUser($username, $password) {
