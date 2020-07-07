d696b6ebbe679454901e43bf23ca3373ee8343ad
Tu Nguyen danh <55817830+tund136@users.noreply.github.com>
2020-07-07 15:36:27 +0700

    Modified admin/includes/init.php file

--- admin/includes/init.php
+++ admin/includes/init.php
@@ -11,5 +11,6 @@ require_once("db_object.php");
 require_once("functions.php"); 
 require_once("user.php");
 require_once("photo.php");
+require_once("comment.php");
 require_once("session.php");
 ?>
