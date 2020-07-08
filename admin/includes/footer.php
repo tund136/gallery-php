c6c6af5b301ce9a95d268adbd14c14dc47ced8ec
Tu Nguyen danh <55817830+tund136@users.noreply.github.com>
2020-07-08 21:48:07 +0700

    Modified admin/includes/footer.php file

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -10,6 +10,33 @@
     <!-- Tinymce Editor -->
     <script src="https://cdn.tiny.cloud/1/slx86oz4k07r6imnyed3jf3y9w9rhpu7pdltaky61e2hmpxx/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     <script src="js/scripts.js"></script>
+
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Views',     <?php echo $session->count; ?>],
+          ['Photos',    <?php echo Photo::countAll(); ?>],
+          ['Users',     <?php echo User::countAll(); ?>],
+          ['Comments',  <?php echo Comment::countAll(); ?>]
+        ]);
+
+        var options = {
+          legend: 'none',
+          pieSliceText: 'label',
+          title: 'My Daily Activities',
+          backgroundColor: 'transparent'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
+    </script>
 </body>
 
 </html>
