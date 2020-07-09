942ea1d43d5035aa237dc2848ed88b3870138c66
Tu Nguyen danh <55817830+tund136@users.noreply.github.com>
2020-07-09 14:26:27 +0700

    Enabling the Selection Button with JQuery

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -9,6 +9,7 @@
 
     <!-- Tinymce Editor -->
     <script src="https://cdn.tiny.cloud/1/slx86oz4k07r6imnyed3jf3y9w9rhpu7pdltaky61e2hmpxx/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+    
     <script src="js/scripts.js"></script>
 
     <script type="text/javascript">
--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -1,9 +1,9 @@
-tinymce.init({
-    selector: 'textarea',
-    plugins: 'a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
-    toolbar: 'a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table',
-    toolbar_mode: 'floating',
-    tinycomments_mode: 'embedded',
-    tinycomments_author: 'Author name',
+$(document).ready(function () {
+    $(".modal_thumbnails").click(function () {
+        $("#set_user_image").prop('disabled', false);
+    });
+
+    tinymce.init({selector: 'textarea'});
 });
+
     
