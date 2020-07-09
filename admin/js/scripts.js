1114601d59b1aba292be0eedb89fabd5a21f8f02
Tu Nguyen danh <55817830+tund136@users.noreply.github.com>
2020-07-09 14:56:09 +0700

    Pulling Image Name with jQuery and JS

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -3,6 +3,11 @@ $(document).ready(function () {
     var user_href_splitted;
     var user_id;
 
+    var image_src;
+    var image_href_splitted;
+    var image_id;
+
+
     // Enabling the Selection button
     $(".modal_thumbnails").click(function () {
         $("#set_user_image").prop('disabled', false);
@@ -10,7 +15,12 @@ $(document).ready(function () {
         user_href = $("#user-id").prop('href');
         user_href_splitted = user_href.split("=");
         user_id = user_href_splitted[user_href_splitted.length - 1];
-        alert(user_id);
+
+        image_src = $(this).prop('src');
+        image_href_splitted = image_src.split("/");
+        image_id = image_href_splitted[image_href_splitted.length - 1];
+
+        alert(image_id);
     });
 
     // TinyMCE
