6b839d7b3d2b07db44bbaf387024b7127702cbf0
Tu Nguyen danh <55817830+tund136@users.noreply.github.com>
2020-07-09 14:03:20 +0700

    Pulling pictures into Gallery Modal

--- admin/edit_user.php
+++ admin/edit_user.php
@@ -1,20 +1,24 @@
 <?php include("includes/header.php"); ?>
-<?php if(!$session->isSignedIn()) {redirect("login.php");} ?>
+<?php include("includes/photo_library_modal.php"); ?>
+
+<?php if (!$session->isSignedIn()) {
+    redirect("login.php");
+} ?>
 
 <?php
 $message = "";
 
-if(empty($_GET['user_id'])) {
+if (empty($_GET['user_id'])) {
     redirect("users.php");
 }
 
 $user = User::findById($_GET['user_id']);
-if(isset($_POST['update'])) {
-    if($user) {
+if (isset($_POST['update'])) {
+    if ($user) {
         $user->first_name = $_POST['first_name'];
         $user->last_name = $_POST['last_name'];
         $user->password = $_POST['password'];
-        if(empty($_FILES['user_image'])) {
+        if (empty($_FILES['user_image'])) {
             $user->save();
         } else {
             $user->setFile($_FILES['user_image']);
@@ -42,53 +46,53 @@ if(isset($_POST['update'])) {
 <div id="page-wrapper">
 
     <div class="container-fluid">
-        
+
         <!-- Page Heading -->
         <div class="row">
             <div class="col-lg-12">
                 <h1 class="page-header">
-                    Users
-                    <small>Subheading</small>
+                    Edit User
+                    <small></small>
                 </h1>
 
                 <div class="col-md-6">
-                    <img class="img-responsive" src="<?php echo $user->imagePathAndPlaceholder(); ?>" alt="">
+                    <a href="#" data-toggle="modal" data-target="#photo-library"><img class="img-responsive" src="<?php echo $user->imagePathAndPlaceholder(); ?>" alt=""></a>
                 </div>
 
                 <div class="col-md-6">
-                        <form action="" method="post" enctype="multipart/form-data">
-                            <?php echo $message; ?>
+                    <form action="" method="post" enctype="multipart/form-data">
+                        <?php echo $message; ?>
 
-                            <div class="form-group">
-                                <label for="username">Username</label>
-                                <input type="text" name="username" class="form-control" value="<?php echo $user->username; ?>" disabled>
-                            </div>
+                        <div class="form-group">
+                            <label for="username">Username</label>
+                            <input type="text" name="username" class="form-control" value="<?php echo $user->username; ?>" disabled>
+                        </div>
 
-                            <div class="form-group">
-                                <label for="user_image">User Image</label>
-                                <input type="file" name="user_image">
-                            </div>
+                        <div class="form-group">
+                            <label for="user_image">User Image</label>
+                            <input type="file" name="user_image">
+                        </div>
 
-                            <div class="form-group">
-                                <label for="first_name">First Name</label>
-                                <input type="text" name="first_name" class="form-control" value="<?php echo $user->first_name; ?>" required>
-                            </div>
+                        <div class="form-group">
+                            <label for="first_name">First Name</label>
+                            <input type="text" name="first_name" class="form-control" value="<?php echo $user->first_name; ?>" required>
+                        </div>
 
-                            <div class="form-group">
-                                <label for="last_name">Last Name</label>
-                                <input type="text" name="last_name" class="form-control" value="<?php echo $user->last_name; ?>" required>
-                            </div>
+                        <div class="form-group">
+                            <label for="last_name">Last Name</label>
+                            <input type="text" name="last_name" class="form-control" value="<?php echo $user->last_name; ?>" required>
+                        </div>
 
-                            <div class="form-group">
-                                <label for="password">Password</label>
-                                <input type="password" name="password" class="form-control" required>
-                            </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" name="password" class="form-control" required>
+                        </div>
 
-                            <a href="delete_user?user_id=<?php echo $user->id; ?>" class="btn btn-danger">Delete</a>
-                            <input type="submit" name="update" class="btn btn-primary" value="Update">
-                        </form>
-                    </div>
-                
+                        <a href="delete_user?user_id=<?php echo $user->id; ?>" class="btn btn-danger">Delete</a>
+                        <input type="submit" name="update" class="btn btn-primary" value="Update">
+                    </form>
+                </div>
+
             </div>
         </div>
         <!-- /.row -->
