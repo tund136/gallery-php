55d35ca8d09be2b2d62814f3b6039d91b903f6c5
Tu Nguyen danh <55817830+tund136@users.noreply.github.com>
2020-07-09 09:26:32 +0700

    Front-end Gallery System

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,4 @@
+<?php require_once("admin/includes/init.php"); ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -23,6 +24,8 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <link href="css/styles.css" rel="stylesheet">
 </head>
 
 <body>
--- index.php
+++ index.php
@@ -1,18 +1,30 @@
 <?php include("includes/header.php"); ?>
 
+<?php
+$photos = Photo::findAll();
+?>
+
 <div class="row">
 
     <!-- Blog Entries Column -->
-    <div class="col-md-8">
-
+    <div class="col-md-12">
+        <div class="thumbnails row">
+            <?php foreach ($photos as $photo): ?>
+            <div class="col-xs-6 col-md-3">
+                <a href="photo_page.php?photo_id=<?php echo $photo->id;?>" class="thumbnail">
+                    <img class="home_page_photo img-responsive" src="admin/<?php echo $photo->picturePath(); ?>" alt="">
+                </a>
+            </div>
+            <?php endforeach; ?>
+        </div>
     </div>
 
     <!-- Blog Sidebar Widgets Column -->
-    <div class="col-md-4">
+    <!-- <div class="col-md-4">
 
-        <?php include("includes/sidebar.php"); ?>
+        <?php // include("includes/sidebar.php"); ?>
 
-    </div>
+    </div> -->
     <!-- /.row -->
 
     <?php include("includes/footer.php"); ?>
